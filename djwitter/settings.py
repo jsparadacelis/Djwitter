37579c1252db6e6edefc6790da4e4ec3169c4ee3
jsparadacelis <parada.celis@gmail.com>
2018-11-23 08:58:36 -0500

    Static and templates for apps

--- djwitter/settings.py
+++ djwitter/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'djwitter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
--- users/models.py
+++ users/models.py
@@ -6,3 +6,7 @@ class Perfil(models.Model):
     bio = models.CharField(max_length=300)
     f_nacimiento = models.DateField(auto_now=False, auto_now_add=False)
     f_creacion = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        return self.user.username
+    
