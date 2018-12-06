5ea04f6fd63350e44d9c0869260fc885cdfc5d0d
jsparadacelis <parada.celis@gmail.com>
2018-12-06 16:37:34 -0500

    Create tweet with request user as param

--- feed/views.py
+++ feed/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Post
+from .models import Tweet
 from users.models import Perfil
 
 
@@ -15,21 +15,24 @@ class list_tweets(ListView):
     #Nombre de la plantilla para la vista
     template_name = "posts/feed.html"
     #Modelo del cual se van a extraer los datos
-    model = Post
+    model = Tweet
     #NOmbre que se le da al diccionario del contexto para mostrar los datos en la plantilla
     context_object_name = "list_tweets"
 
 
 def send_tweet(request):
-    #perfil de prueba
-    perfil = request.user.perfil
+    
+    print(request.user.perfil)
     if request.method == 'POST':
         form = TweetForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data["content"]
-            post = Post(perfil = perfil, texto = data)
-            post.save()
-            return redirect('/feed/hi')
+            tweet = Tweet(
+                perfil = request.user.perfil, 
+                texto = data
+            )
+            tweet.save()
+            return redirect('list_tweets')
 
     else:
         form = TweetForm()
