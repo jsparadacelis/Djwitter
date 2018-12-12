02dce555b013f4902239acd344ca40b60cfa28ee
jsparadacelis <parada.celis@gmail.com>
2018-12-11 22:07:19 -0500

    Support in user model for followers

--- users/admin.py
+++ users/admin.py
@@ -3,6 +3,6 @@ from .models import Perfil
 
 # Register your models here.
 class PerfilAdmin(admin.ModelAdmin):
-    fields = ['user','bio','f_nacimiento','foto_perfil']
+    fields = ['user','bio','f_nacimiento','foto_perfil', 'seguidores']
 
 admin.site.register(Perfil, PerfilAdmin)
